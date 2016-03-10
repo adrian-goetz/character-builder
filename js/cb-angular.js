8331e6b5b7db068242335aaf7005bf60c0ffea45
Adrian Goetz <adrian-goetz@users.noreply.github.com>
2016-03-10 18:46:08 -0500

    Skeleton built with angular

--- js/cb-angular.js
+++ js/cb-angular.js
@@ -8,6 +8,71 @@ app.controller('mainCtrl', ['$scope', function($scope) {
 		$scope.page = newPage;
 	}
 
+    $scope.char = {};
+
+    $scope.races = [
+        {
+            name: "dwarf",
+            subraces: [],
+            options: []
+        }, 
+        {
+            name: "elf",
+            subraces: [],
+            options: []
+        }, 
+        {
+            name: "halfling",
+            subraces: [],
+            options: []
+        }, 
+        {
+            name: "human",
+            subraces: [null],
+            options: []
+        }, 
+        {
+            name: "dragonborn",
+            subraces: [null],
+            options: []
+        }, 
+        {
+            name: "gnome",
+            subraces: [],
+            options: []
+        }, 
+        {
+            name: "half-elf",
+            subraces: [null],
+            options: []
+        }, 
+        {
+            name: "half-orc",
+            subraces: [null],
+            options: []
+        }, 
+        {
+            name: "gnome",
+            subraces: [],
+            options: []
+        }, 
+        {
+            name: "Aarakocra",
+            subraces: [null],
+            options: []
+        }, 
+        {
+            name: "genasi",
+            subraces: [],
+            options: []
+        }, 
+        {
+            name: "goliath",
+            subraces: [null],
+            options: []
+      }
+    ]
+
     // Standard Array
     // [15, 14, 13, 12, 10, 8]
 
